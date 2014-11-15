a0ad0f1d011d0a444f07b69209d173bd9bca5cad
Sabba Petri <sabbap@gmail.com>
2014-11-15 11:01:08 -0800

    Added some logic to determine query state

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,12 @@
 from haystack.forms import SearchForm
 
 class BooksSearchForm(SearchForm):
+  
   def no_query_found(self):
     return self.searchqueryset.all()
+
+  def search(self):
+    sqs = super(BooksSearchForm, self).search()
+    if not self.is_valid():
+      return self.no_query_found()
+    return sqs.order_by('title')
--- books/views.py
+++ books/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render_to_response
 from .forms import BooksSearchForm
+from haystack.query import SearchQuerySet
+from haystack.utils import Highlighter
 
 def books(request):
-  form = BooksSearchForm(request.GET)
-  books = form.search()
-  return render_to_response('books.html', {'books': books})
+  data = request.GET
+  form = BooksSearchForm(data)
+  if not data or data['q'] == '':
+    books = form.search()
+    highlighted_books = form.search().highlight()[0].highlighted
+    return render_to_response('books.html',
+      {
+        'books': books,
+      }
+    )
+  else:
+    books = form.search()
+    highlighted_books = form.search().highlight()[0].highlighted
+    return render_to_response('books.html',
+      {
+        'books': highlighted_books,
+      }
+    )
