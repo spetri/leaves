6558da28318375348dcd6704e975e370bfd094b1
Sabba Petri <sabbap@gmail.com>
2014-11-16 18:51:33 -0800

    Begun HTML/CSS work: Sidebar in-progress
    HTML/CSS templates have been put in place
    and preliminary work has begun on the sidebar.
    3 windows have also been created, with results
    displaying in the second window.

--- books/views.py
+++ books/views.py
@@ -6,19 +6,8 @@ from haystack.utils import Highlighter
 def books(request):
   data = request.GET
   form = BooksSearchForm(data)
+  highlighted_books = form.search().highlight()[0].highlighted
   if not data or data['q'] == '':
-    books = form.search()
-    highlighted_books = form.search().highlight()[0].highlighted
-    return render_to_response('books.html',
-      {
-        'books': books,
-      }
-    )
+    return render_to_response('books.html')
   else:
-    books = form.search()
-    highlighted_books = form.search().highlight()[0].highlighted
-    return render_to_response('books.html',
-      {
-        'books': highlighted_books,
-      }
-    )
+    return render_to_response('books.html', { 'books': highlighted_books })
