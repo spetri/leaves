058febc10563f5cf2d50c9f9a04d1f5df554cdaf
Sabba Petri <sabbap@gmail.com>
2014-11-17 22:48:56 -0800

    The JavaScript has begun.
    Added some JS to bring functionality of second and third pane.

--- books/static/js/jq-leaves.js
+++ books/static/js/jq-leaves.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
-  $('#submit').click(function() {
-    console.log("clicked");
-  });
+  
+  function docViewerHeader() {
+    var tableInfo = getTableInformation($(this));
+    var header = $('#docviewer-view div:first-child');
+    header.addClass('docviewer-header');
+    header.find('span:first-child').addClass('docviewer-title').text(tableInfo.title);
+    header.find('span:nth-child(3)').addClass('docviewer-author').text(tableInfo.author);
+  };
+
+  function getTableInformation(row) {
+    var title = row.find("td:first-child").text();
+    var author = row.find("td:nth-child(2)").text();
+    return {title: title, author: author};
+  };
+
+  function selectRow() {
+    if($(this).hasClass('selected')) {
+      $(this).removeClass('selected');
+    } else {
+      $(this).addClass('selected');
+      $(this).siblings().removeClass('selected');
+    };
+  };
+
+$('#results-view tr').on("click", docViewerHeader);
+$('#results-view tr').on("click", selectRow);
 
 });
